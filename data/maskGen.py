961e1aab78ae6efa4edbd255952bfd03b8ddeeae
Arnaud <aruno14@outlook.com>
2021-01-25 19:44:59 +0900

    Init

---
+++ data/maskGen.py
@@ -0,0 +1,53 @@
+import os
+import cv2
+import json
+import numpy as np
+
+source_folder = os.path.join(os.getcwd(), "images")
+mask_folder = os.path.join(os.getcwd(), "masks")
+if not os.path.exists(mask_folder):
+    os.mkdir(mask_folder)
+json_path = "food_json.json"
+count = 0
+file_bbs = {}
+
+with open(json_path) as f:
+  data = json.load(f)
+
+for itr in data:
+    file_name_json = data[itr]["filename"]
+    print("file_name_json", file_name_json)
+    file_bbs[file_name_json] = data[itr]["regions"]
+
+print("\nDict size: ", len(file_bbs))
+
+for file_name in file_bbs:
+    print(file_name)
+    original_image = cv2.imread(os.path.join(source_folder, file_name))
+    mask = np.zeros((original_image.shape[0], original_image.shape[1]))
+    print(file_name)
+    for shape in file_bbs[file_name]:
+        try:
+            type = shape['region_attributes']['categorie']
+            x_points = shape["shape_attributes"]["all_points_x"]
+            y_points = shape["shape_attributes"]["all_points_y"]
+
+            all_points = []
+            for i, x in enumerate(x_points):
+                all_points.append([x, y_points[i]])
+            arr = np.array(all_points)
+            if type == "assiette":
+                color = (255)
+            elif type == "verre":
+                color = (200)
+            else:
+                color = (100)
+            cv2.fillPoly(mask, [arr], color=color)
+        except:
+            print("Not found:", file_name)
+            continue
+    print("write mask", os.path.join(mask_folder, file_name.replace(".jpg", ".png")))
+    cv2.imwrite(os.path.join(mask_folder, file_name.replace(".jpg", ".png")) , mask)
+    count+=1
+
+print("Images saved:", count)
---
+++ train_seg_food.py
@@ -0,0 +1,116 @@
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Activation, Lambda, GlobalAveragePooling2D, concatenate
+from tensorflow.keras.layers import UpSampling2D, Conv2D, Dropout, MaxPooling2D, Conv2DTranspose
+from tensorflow.keras.layers import Dense, Flatten, Input
+from tensorflow.keras.models import Model, Sequential, load_model
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from glob import glob
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+IMG_HEIGHT=600
+IMG_WIDTH=800
+
+x_files = glob('myData/images/*')
+y_files = glob('myData/masks/*')
+
+files_ds = tf.data.Dataset.from_tensor_slices((x_files, y_files))
+
+def process_img(file_path:str, channels=3):
+    img = tf.io.read_file(file_path)
+    img = tf.image.decode_jpeg(img, channels=channels)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    img = tf.image.resize(img, size=(IMG_HEIGHT, IMG_WIDTH))
+    return img
+
+files_ds = files_ds.map(lambda x, y: (process_img(x), process_img(y, 1))).batch(1)
+
+def get_model(IMG_HEIGHT, IMG_WIDTH):
+    in1 = Input(shape=(IMG_HEIGHT, IMG_WIDTH, 3))
+
+    conv1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(in1)
+    conv1 = Dropout(0.2)(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(pool1)
+    conv2 = Dropout(0.2)(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(pool2)
+    conv3 = Dropout(0.2)(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(conv3)
+    pool3 = MaxPooling2D((2, 2))(conv3)
+
+    conv4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(pool3)
+    conv4 = Dropout(0.2)(conv4)
+    conv4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(conv4)
+
+    up1 = concatenate([UpSampling2D((2, 2))(conv4), conv3], axis=-1)
+    conv5 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(up1)
+    conv5 = Dropout(0.2)(conv5)
+    conv5 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(conv5)
+
+    up2 = concatenate([UpSampling2D((2, 2))(conv5), conv2], axis=-1)
+    conv6 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(up2)
+    conv6 = Dropout(0.2)(conv6)
+    conv6 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(conv6)
+
+    up2 = concatenate([UpSampling2D((2, 2))(conv6), conv1], axis=-1)
+    conv7 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(up2)
+    conv7 = Dropout(0.2)(conv7)
+    conv7 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(conv7)
+    segmentation = Conv2D(1, (1, 1), activation='sigmoid', name='seg')(conv7)
+
+    model = Model(inputs=[in1], outputs=[segmentation])
+
+    losses = {'seg': 'binary_crossentropy'}
+
+    metrics = {'seg': ['acc']}
+    model.compile(optimizer="adam", loss = losses, metrics=metrics)
+
+    return model
+
+model_name = "traySegModel"
+if os.path.exists(model_name):
+    model = tf.keras.models.load_model(model_name)
+else:
+    model = get_model(IMG_HEIGHT, IMG_WIDTH)
+
+model.summary()
+tf.keras.utils.plot_model(model, show_shapes=True)
+
+def showPrediction(imagePath:str, count=0):
+    test_image = process_img("myData/images/assiette-01.jpg")
+    predict = model.predict(np.asarray([test_image]))[0]
+
+    fig, axes= plt.subplots(nrows=2, ncols=1)
+    axes[0].imshow(test_image)
+    axes[1].imshow(predict)
+    plt.savefig("currentResult-"+str(count)+".png")
+    plt.close()
+
+class DisplayCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs=None):
+    showPrediction("myData/images/assiette-01.jpg", epoch)
+    print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
+
+epochs = 5
+print(files_ds)
+history = model.fit(files_ds, epochs=epochs, callbacks=[DisplayCallback()])
+model.save(model_name)
+
+metrics = history.history
+
+plt.plot(history.epoch, metrics['loss'], metrics['acc'])
+plt.legend(['loss', 'acc'])
+plt.savefig("learning-emotion.png")
+plt.show()
+plt.close()
+
+#for image_path in ["myData/images/assiette-01.jpg", "myData/images/choucroute-01.jpg"]:
+#    showPrediction(image_path)
