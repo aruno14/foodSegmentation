4548c076add8c1ab51ce7f67fd4af8ab2643635c
Arnaud <aruno14@outlook.com>
2021-01-26 18:00:06 +0900

    Add TensorBoard and test with another dataset

--- train_seg_food.py
+++ train_seg_food.py
@@ -1,3 +1,8 @@
+import os
+from glob import glob
+from datetime import datetime
+import numpy as np
+
 import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Activation, Lambda, GlobalAveragePooling2D, concatenate
@@ -5,33 +10,40 @@ from tensorflow.keras.layers import UpSampling2D, Conv2D, Dropout, MaxPooling2D,
 from tensorflow.keras.layers import Dense, Flatten, Input
 from tensorflow.keras.models import Model, Sequential, load_model
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
-from glob import glob
 
 import matplotlib.pyplot as plt
-import numpy as np
-import os
+from PIL import Image
 
-IMG_HEIGHT=600
-IMG_WIDTH=800
+#IMG_HEIGHT=600
+#IMG_WIDTH=800
+IMG_HEIGHT=416
+IMG_WIDTH=256
 
-x_files = glob('myData/images/*')
-y_files = glob('myData/masks/*')
+#x_files = glob('myData/images/*')
+#y_files = glob('myData/masks/*')
+x_files = glob('TrayDataset/TrayDataset/XTrain/*')
+y_files = glob('TrayDataset/TrayDataset/yTrain/*')
 
 files_ds = tf.data.Dataset.from_tensor_slices((x_files, y_files))
 
-def process_img(file_path:str, channels=3):
+def process_img(file_path:str, channels=3, diviser=1):
     img = tf.io.read_file(file_path)
     img = tf.image.decode_jpeg(img, channels=channels)
     img = tf.image.convert_image_dtype(img, tf.float32)
-    img = tf.image.resize(img, size=(IMG_HEIGHT, IMG_WIDTH))
+    img = tf.image.resize(img, size=(IMG_HEIGHT//diviser, IMG_WIDTH//diviser))
     return img
 
-files_ds = files_ds.map(lambda x, y: (process_img(x), process_img(y, 1))).batch(1)
+files_ds = files_ds.map(lambda x, y: (process_img(x), process_img(y, channels=3, diviser=2))).batch(1)
+
 
 def get_model(IMG_HEIGHT, IMG_WIDTH):
     in1 = Input(shape=(IMG_HEIGHT, IMG_WIDTH, 3))
+    preprop = tf.keras.layers.experimental.preprocessing.Resizing(IMG_HEIGHT//2, IMG_WIDTH//2)(in1)
+    preprop = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)(preprop)
+    preprop = tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical")(preprop)
+    preprop = tf.keras.layers.experimental.preprocessing.RandomRotation(0.2)(preprop)
 
-    conv1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(in1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(preprop)#(in1)
     conv1 = Dropout(0.2)(conv1)
     conv1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(conv1)
     pool1 = MaxPooling2D((2, 2))(conv1)
@@ -64,11 +76,12 @@ def get_model(IMG_HEIGHT, IMG_WIDTH):
     conv7 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(up2)
     conv7 = Dropout(0.2)(conv7)
     conv7 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(conv7)
-    segmentation = Conv2D(1, (1, 1), activation='sigmoid', name='seg')(conv7)
+    segmentation = Conv2D(3, (1, 1), activation='sigmoid', name='seg')(conv7)
 
     model = Model(inputs=[in1], outputs=[segmentation])
 
     losses = {'seg': 'binary_crossentropy'}
+    #losses = {'seg': 'mean_squared_error'}
 
     metrics = {'seg': ['acc']}
     model.compile(optimizer="adam", loss = losses, metrics=metrics)
@@ -85,30 +98,43 @@ model.summary()
 tf.keras.utils.plot_model(model, show_shapes=True)
 
 def showPrediction(imagePath:str, count=0):
-    test_image = process_img("myData/images/assiette-01.jpg")
-    predict = model.predict(np.asarray([test_image]))[0]
+    test_image = process_img(imagePath)
+    predictions = model.predict(np.asarray([test_image]))
+    predictions *= 255
+    predicted_image = predictions[0]
 
-    fig, axes= plt.subplots(nrows=2, ncols=1)
-    axes[0].imshow(test_image)
-    axes[1].imshow(predict)
-    plt.savefig("currentResult-"+str(count)+".png")
-    plt.close()
+    #im = Image.fromarray(np.squeeze(predicted_image), mode="L")
+    print(predicted_image.shape)
+    im = Image.fromarray(predicted_image, mode="RGB")
+    im.save("predict-"+str(count)+".png")
+
+    return test_image, predictions
 
 class DisplayCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs=None):
-    showPrediction("myData/images/assiette-01.jpg", epoch)
-    print ('\nSample Prediction after epoch {}\n'.format(epoch+1))
+    #test_image, predictions = showPrediction("myData/images/assiette-01.jpg", epoch)
+    test_image, predictions = showPrediction("TrayDataset/TrayDataset/XTrain/image-1001a01.jpg", epoch)
+
+    file_writer = tf.summary.create_file_writer(log_dir)
+
+    with file_writer.as_default():
+      tf.summary.image("Training data", predictions, step=epoch)
+      tf.summary.image("Input data", [test_image], step=epoch)
+
+log_dir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 epochs = 5
 print(files_ds)
-history = model.fit(files_ds, epochs=epochs, callbacks=[DisplayCallback()])
+#tensorboard --logdir logs/
+history = model.fit(files_ds, epochs=epochs, steps_per_epoch=100, callbacks=[tensorboard_callback, DisplayCallback()])#, steps_per_epoch=10
 model.save(model_name)
 
 metrics = history.history
 
 plt.plot(history.epoch, metrics['loss'], metrics['acc'])
 plt.legend(['loss', 'acc'])
-plt.savefig("learning-emotion.png")
+plt.savefig("fit-history.png")
 plt.show()
 plt.close()
 
